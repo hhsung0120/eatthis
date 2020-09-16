b94ffcdef83e369a001a9d04b709a05027ff967f
hshan <hhsung0120@naver.com>
2020-09-16 22:49:00 +0900

    메인 리스트 수정

--- src/main/java/co/kr/heeseong/eatthis/controller/ApiDescriptionController.java
+++ src/main/java/co/kr/heeseong/eatthis/controller/ApiDescriptionController.java
@@ -1,25 +1,15 @@
 package co.kr.heeseong.eatthis.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
+@Controller
 @RequestMapping("/api")
-@RequiredArgsConstructor
 public class ApiDescriptionController {
 
     @GetMapping("/description")
-    public Map<String, Object> mainList(){
-        Map<String, Object> result = new HashMap<String, Object>();
-
-        result.put("[GET] /main/list","메인 리스트");
-        result.put("[GET] /user/myPage/{userIdx}","마이페이지");
-
-        return result;
+    public String mainList(){
+        return "index";
     }
 }
