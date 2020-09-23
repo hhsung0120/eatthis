ca009aeabf00acfd14526e65f4040d47c73e9f74
hshan <hhsung0120@naver.com>
2020-09-24 00:57:23 +0900

    포트 처리

--- src/main/java/co/kr/heeseong/eatthis/controller/IndexController.java
+++ src/main/java/co/kr/heeseong/eatthis/controller/IndexController.java
@@ -1,17 +1,24 @@
 package co.kr.heeseong.eatthis.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping("")
     public String index(HttpServletRequest request){
+        log.info("request {}", request.getRequestURI());
+        log.info("request {}", request.getRequestURL());
+        log.info("request {}", request.getServerPort());
+        log.info("request {}", request.getServletContext());
+        log.info("request {}", request.getServletPath());
         return "redirect:"+request.getRequestURL()+"/api/description";
     }
 }
