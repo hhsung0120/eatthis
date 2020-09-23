47a76443c34c4aaa25d3af9c80ebe5c33f73395c
hshan <hhsung0120@naver.com>
2020-09-24 00:51:30 +0900

    포트 처리

--- src/main/java/co/kr/heeseong/eatthis/controller/IndexController.java
+++ src/main/java/co/kr/heeseong/eatthis/controller/IndexController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping("")
-    public String index(){
-        return "redirect:/api/description";
+    public String index(HttpServletRequest request){
+        return "redirect:"+request.getRequestURL()+"/api/description";
     }
 }
