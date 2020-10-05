3b053caeb5449a8e67e02d2fa838948cec100564
hshan <hhsung0120@naver.com>
2020-10-05 17:29:44 +0900

    faq 작업중

--- src/main/java/co/kr/heeseong/eatthis/controller/FaqController.java
+++ src/main/java/co/kr/heeseong/eatthis/controller/FaqController.java
@@ -1,12 +1,15 @@
 package co.kr.heeseong.eatthis.controller;
 
+import co.kr.heeseong.eatthis.dto.FaqDto;
 import co.kr.heeseong.eatthis.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -24,8 +27,9 @@ public class FaqController {
     }
 
 
-    @GetMapping("/list")
-    public Map<String, Object> faqList(){
+    @GetMapping("/list/{page}")
+    public Map<String, Object> faqList(@PathVariable int page){
+        List<FaqDto> faqList = faqService.getFaqList(page);
         return new HashMap<>();
     }
 
--- src/main/java/co/kr/heeseong/eatthis/service/FaqService.java
+++ src/main/java/co/kr/heeseong/eatthis/service/FaqService.java
@@ -1,15 +1,37 @@
 package co.kr.heeseong.eatthis.service;
 
+import co.kr.heeseong.eatthis.domain.faq.FaqEntity;
+import co.kr.heeseong.eatthis.domain.faq.FaqRepository;
+import co.kr.heeseong.eatthis.dto.FaqDto;
 import co.kr.heeseong.eatthis.dto.NoticeDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class FaqService {
 
+    private FaqRepository faqRepository;
+    private static int pageSize = 10;
+
     public Long insertFaq(NoticeDto noticeDto) {
         return 3L;
+    }
+
+    public List<FaqDto> getFaqList(int page) {
+        List<FaqDto> faqDtoList = new ArrayList<>();
+        Page<FaqEntity> faqEntitieList = faqRepository.findAll(PageRequest.of((page-1), pageSize, Sort.Direction.DESC,"idx"));
+
+        for(FaqEntity faqEntity : faqEntitieList){
+            FaqDto faqDto = FaqDto.builder()
+                    .
+        }
     }
 /*
     public List<NoticeDto> getNoticeList(int page) {
